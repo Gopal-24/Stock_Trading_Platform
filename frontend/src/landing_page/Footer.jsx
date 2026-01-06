0f058541299943e2ec7618ba9d7c6585223fcfb4
Gopal-24 <megopalkhamnekar@gmail.com>
2026-01-06 21:43:20 +0530

    Designed the Universal Components

--- frontend/src/landing_page/Footer.jsx
+++ frontend/src/landing_page/Footer.jsx
@@ -1,7 +1,132 @@
 import React from 'react'
 
-function Footer() {
-    return ( <h1>Footer</h1> );
+function Footer () {
+  return (
+    <div className='container border-top mt-5'>
+      <div className='row mt-5'>
+        <div className='col'>
+          <img src='media/images/logo.svg' style={{ width: '50%' }} alt='' />
+          <p className='mt-3'>
+            &copy; 2010 - 2024, Not Zerodha Booking Ltd. All rights reserved.
+          </p>
+        </div>
+        <div className='col'>
+          <p>Company</p>
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            About
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Products
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Pricing
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Referral programme
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Zerodha.tech
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Press & media
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Zerodha cares (CSR)
+          </a>
+          <br />
+        </div>
+        <div className='col'>
+          <p>Support</p>
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Contact
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Support portal
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Z-Connect blog
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            List of charges
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Downloads and resources
+          </a>
+          <br />
+        </div>
+        <div className='col'>
+          <p>Account</p>
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Open an account
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            Fund transfer
+          </a>
+          <br />
+          <a href='' className='d-block mb-1 text-decoration-none text-muted'>
+            60 day challenge
+          </a>
+          <br />
+        </div>
+      </div>
+      <div className='mt-3 fs-6 footer-text'>
+        <p>
+          Zerodha Broking Ltd.: Member of NSE & BSE – SEBI Registration no.:
+          INZ000031633 CDSL: Depository services through Zerodha Securities Pvt.
+          Ltd. – SEBI Registration no.: IN-DP-100-2015 Commodity Trading through
+          Zerodha Commodities Pvt. Ltd. MCX: 46025 – SEBI Registration no.:
+          INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th
+          Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th
+          Phase, Bengaluru - 560078, Karnataka, India. For any complaints
+          pertaining to securities broking please write to
+          complaints@zerodha.com, for DP related to dp@zerodha.com. Please
+          ensure you carefully read the Risk Disclosure Document as prescribed
+          by SEBI | ICF
+        </p>
+
+        <p>
+          Procedure to file a complaint on SEBI SCORES: Register on SCORES
+          portal. Mandatory details for filing complaints on SCORES: Name, PAN,
+          Address, Mobile Number, E-mail ID. Benefits: Effective Communication,
+          Speedy redressal of the grievances
+        </p>
+
+        <p>
+          Investments in securities market are subject to market risks; read all
+          the related documents carefully before investing.
+        </p>
+
+        <p>
+          "Prevent unauthorised transactions in your account. Update your mobile
+          numbers/email IDs with your stock brokers. Receive information of your
+          transactions directly from Exchange on your mobile/email at the end of
+          the day. Issued in the interest of investors. KYC is one time exercise
+          while dealing in securities markets - once KYC is done through a SEBI
+          registered intermediary (broker, DP, Mutual Fund etc.), you need not
+          undergo the same process again when you approach another
+          intermediary." Dear Investor, if you are subscribing to an IPO, there
+          is no need to issue a cheque. Please write the Bank account number and
+          sign the IPO application form to authorize your bank to make payment
+          in case of allotment. In case of non allotment the funds will remain
+          in your bank account. As a business we don't give stock tips, and have
+          not authorized anyone to trade on behalf of others. If you find anyone
+          claiming to be part of Zerodha and offering such services, please
+          create a ticket here.
+        </p>
+      </div>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-function OpenAccount() {
-    return ( <h1>OpenAccount</h1> );
+function OpenAccount () {
+  return (
+    <div className='container p-5 mb-5'>
+      <div className='row text-center'>
+        <h1 className='mt-5 fs-2 mb-3'>Open a Zerodha account</h1>
+        <p className='mb-3 text-muted'>
+          Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+          F&O trades.
+        </p>
+        <button className='btn btn-primary col-2 m-auto mt-3 p-2 fs-5'>
+          Sign up now
+        </button>
+      </div>
+    </div>
+  )
 }
 
-export default OpenAccount;
+export default OpenAccount
