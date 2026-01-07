aad08e90136ef272adb4527e2e62dd35f4e6ff3d
Gopal-24 <megopalkhamnekar@gmail.com>
2026-01-07 20:18:33 +0530

    Designed Basic Dashboard Structure

---
+++ dashboard/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Dashboard from './Dashboard'
+import TopBar from './Topbar'
+
+function Home () {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  )
+}
+
+export default Home
