d330aaea01eaf123a7f7b6d7ff1bdae07211b97a
Gopal-24 <megopalkhamnekar@gmail.com>
2026-01-08 21:54:30 +0530

    Establish Database Connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+const mongo_url = process.env.MONGO_URL;
 
 const app = express();
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("app started");
+  mongoose.connect(mongo_url);
+  console.log("db connected");
 });
