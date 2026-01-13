e5dbbb04d9879694e53bc8afc8b1dea2a3513c7f
Gopal-24 <megopalkhamnekar@gmail.com>
2026-01-13 12:59:13 +0530

    Added holdings & watchlist charts visualization and configured custom dev ports

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 
 import Tooltip from '@mui/material/Tooltip'
 import Grow from '@mui/material/Grow'
@@ -10,9 +10,38 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 import { watchlist } from '../data/data'
 
 import GeneralContext from './GeneralContext'
-import { useContext } from 'react'
+import DoughnutChart from './DoughnutChart'
+
+const labels = watchlist.map(stock => stock.name)
 
 const WatchList = () => {
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Price',
+        data: watchlist.map(stock => stock.price),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(75, 192, 192, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(255, 159, 64, 0.7)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
+
   return (
     <div className='watchlist-container'>
       <div className='search-container'>
@@ -31,6 +60,8 @@ const WatchList = () => {
           <WatchListItem stock={stock} key={index} />
         ))}
       </ul>
+
+      <DoughnutChart data={data} />
     </div>
   )
 }
